d668405aadbe118bfd59a726ec858fe3cd581cef
frederikbache <mail@frederikbache.dk>
2023-05-01 14:00:11 +0200

    Add extra vrooms to devtools (#35)

--- src/devtools/index.ts
+++ src/devtools/index.ts
@@ -2,7 +2,16 @@ import { setupDevtoolsPlugin } from '@vue/devtools-api';
 
 const LAYER_ID = 'vroom';
 
+let models = [] as any[];
+let collections = {} as any;
+let created = false;
+
 export default function setupDevtools(app: any, db: any, server: any) {
+  models.push(...Object.entries(db));
+  models.forEach(([model, value]) => {
+    collections[model] = value.items;
+  });
+
   setupDevtoolsPlugin(
     {
       id: 'vue-vroom',
@@ -13,71 +22,6 @@ export default function setupDevtools(app: any, db: any, server: any) {
     },
     (api) => {
       const INSPECTOR_ID = 'vue-vroom';
-      api.addInspector({
-        id: INSPECTOR_ID,
-        label: 'Vroom',
-        icon: 'electric_moped',
-      });
-
-      api.addTimelineLayer({
-        id: INSPECTOR_ID,
-        color: 0xff984f,
-        label: 'Vroom',
-      });
-
-      api.on.getInspectorTree((payload, context) => {
-        if (payload.inspectorId !== INSPECTOR_ID) return;
-        payload.rootNodes = [
-          {
-            id: 'db',
-            label: 'Database',
-            children: Object.entries(db).map(([model, collection]) => {
-              return {
-                id: model,
-                label: model,
-              };
-            }),
-          },
-          {
-            id: 'routes',
-            label: 'Routes',
-            children: Object.keys(db).map((model) => {
-              return {
-                id: 'routes:' + model,
-                label: model,
-              };
-            }),
-          },
-        ];
-      });
-
-      api.on.getInspectorState((payload, context) => {
-        if (payload.inspectorId !== INSPECTOR_ID) return;
-        const routeMatch = payload.nodeId.match(/routes:(.*)/);
-        if (payload.nodeId in db) {
-          payload.state = {
-            'DB items': db[payload.nodeId].items.map((item: any) => ({
-              key: item.id,
-              value: item,
-            })),
-          };
-        }
-        if (routeMatch) {
-          payload.state = {
-            Filters: server.filters[routeMatch[1]] || {},
-            Routes: [
-              {
-                key: 'routes',
-                value: server.routes
-                  .filter((route: any) => route.model === routeMatch[1])
-                  .map((route: any) => {
-                    return route.method + ' ' + route.path;
-                  }),
-              },
-            ],
-          };
-        }
-      });
 
       server.setDevTools((settings: any) => {
         setTimeout(() => {
@@ -110,6 +54,50 @@ export default function setupDevtools(app: any, db: any, server: any) {
           });
         };
       });
+
+      if (!created) {
+        created = true;
+        api.addInspector({
+          id: INSPECTOR_ID,
+          label: 'Vroom',
+          icon: 'electric_moped',
+        });
+
+        api.addTimelineLayer({
+          id: INSPECTOR_ID,
+          color: 0xff984f,
+          label: 'Vroom',
+        });
+
+        api.on.getInspectorTree((payload, context) => {
+          if (payload.inspectorId !== INSPECTOR_ID) return;
+          payload.rootNodes = [
+            {
+              id: 'db',
+              label: 'Database',
+              children: models.map(([model, collection]) => {
+                return {
+                  id: model,
+                  label: model,
+                };
+              }),
+            },
+          ];
+        });
+
+        api.on.getInspectorState((payload, context) => {
+          if (payload.inspectorId !== INSPECTOR_ID) return;
+          const routeMatch = payload.nodeId.match(/routes:(.*)/);
+          if (payload.nodeId in collections) {
+            payload.state = {
+              'DB items': collections[payload.nodeId].map((item: any) => ({
+                key: item.id,
+                value: item,
+              })),
+            };
+          }
+        });
+      }
     }
   );
 }
